e46fdefceeee4836d287c5bc947ef37aad3eee52
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-27 13:57:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-const driver = {name: 'nawar', address: 'iraq'}
+const driver = {name: 'Sam', address: 'Broadway'}
 function updateDriverWithKeyAndValue(driver, key, value) {
   driver.name = 'Sam'
   return driver

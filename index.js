2e67f419b05767813ce48e9bcb383567ba18ea01
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-29 14:17:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+const driver = {name: 'Sam'}
+function updateDriverWithKeyAndValue(driver, key, value) {
+
+  driver.name = 'Sam'
+  driver['address'] = '11 Broadway'
+  return driver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver.name = 'Sam'
+  driver['address'] = '12 Broadway'
+  return driver
+}

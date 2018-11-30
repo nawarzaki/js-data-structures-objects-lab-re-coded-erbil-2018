9d4d065c51c1dbe71ffedb61a089421814c84392
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-30 12:57:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const driver = {name: 'nawar', address:'qraqoush'}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = {...driver}
+  newDriver.name = 'Sam'
+  newDriver.address = '11 Broadway'
+  return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver.name = 'Sam'
+  driver['address'] = '12 Broadway'
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+let newDriver = {...driver}
+delete newDriver.name;
+return newDriver
+
+}
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  let newdriver = driver
+  delete driver.name;
+    return newdriver
+
+}
